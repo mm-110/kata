1d1579322c68ad697dafdf3419855592fc16e91e
mm-110 <massimomontanaro.1993@gmail.com>
2025-04-30 00:51:01 +0200

    ADD logic to create chars counter hashmaps

--- anagram/src/main.rs
+++ anagram/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn read_txt(filepath: &str) -> String {
     let content = fs::read_to_string(filepath)
@@ -14,34 +14,52 @@ fn read_txt(filepath: &str) -> String {
 //     string.trim().to_string()
 // }
 
-fn process_string(string: &str) -> Vec<String> {
-    string.split("  ")
-        .map(|s| s.trim().to_string())
-        .filter(|s| &s.to_string() != "")
-        .collect()
-}
+// fn process_string(string: &str) -> Vec<String> {
+//     string.split("  ")
+//         .map(|s| s.trim().to_string())
+//         .filter(|s| &s.to_string() != "")
+//         .collect()
+// }
+
+// TODO
+// funzione che confronta due hashmap, ritorna true se il secondo è contenuto nel primo
 
 fn main() {
     
     let filepath = String::from("vocabulary.txt");
     let content = read_txt(&filepath);
+
+    let start_word = "documenting"; 
+    let mut start_word_letters_count: HashMap<char, u32> = HashMap::new();
+
+    for ch in start_word.chars() {
+        *start_word_letters_count.entry(ch).or_insert(0) += 1;
+    }
     
-    let vocab: Vec<String> = content.split("  ")
+    let vocabs: Vec<String> = content.split("  ")
         .map(|s| s.trim().to_string())
         .filter(|s| &s.to_string() != "")
         .collect();
 
-    println!("{:?}", vocab);
+    for vocab in &vocabs {
+        let mut vocab_letters_count: HashMap<char, u32> = HashMap::new();
+        for vocab_char in vocab.chars() {
+            *vocab_letters_count.entry(vocab_char).or_insert(0) += 1;
+        }
+
+    }
+
+    println!("{:?}", vocabs);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_process_string() {
-        assert_eq!(process_string("a  b  \nc"), ["a", "b", "c"])
-    }
+    // #[test]
+    // fn test_process_string() {
+    //     assert_eq!(process_string("a  b  \nc"), ["a", "b", "c"])
+    // }
 
     // TODO cercare il modo in cui si verifica che fallisca
 }
