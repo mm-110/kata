c33c738b1bffbd5e0e04aba5b629262d000b43e5
mm-110 <massimomontanaro.1993@gmail.com>
2025-05-06 00:51:49 +0200

    ADD compare hashmap function

--- anagram/src/main.rs
+++ anagram/src/main.rs
@@ -6,6 +6,16 @@ fn read_txt(filepath: &str) -> String {
     return content;
 }
 
+fn is_subset(first: &HashMap<char, u32>, second: &HashMap<char, u32>) -> bool {
+    for (key, value) in second {
+        match first.get(key) {
+            Some(v) if v >= value => continue,
+            _ => return false,
+        }
+    }
+    true
+}
+
 // fn split(string: &String, by: &str) -> Vec<String> {
 //     string.split(by).map(|s| s.to_string()).collect()
 // }
@@ -27,7 +37,6 @@ fn read_txt(filepath: &str) -> String {
 fn main() {
     
     let filepath = String::from("vocabulary.txt");
-    let content = read_txt(&filepath);
 
     let start_word = "documenting"; 
     let mut start_word_letters_count: HashMap<char, u32> = HashMap::new();
@@ -36,25 +45,31 @@ fn main() {
         *start_word_letters_count.entry(ch).or_insert(0) += 1;
     }
     
-    let vocabs: Vec<String> = content.split("  ")
+    let vocabs: Vec<String> = read_txt(&filepath)
+        .split("  ")
         .map(|s| s.trim().to_string())
         .filter(|s| &s.to_string() != "")
         .collect();
 
-    for vocab in &vocabs {
+    let mut filtered_vocabs: Vec<String> = Vec::new();
+
+    for vocab in vocabs {
         let mut vocab_letters_count: HashMap<char, u32> = HashMap::new();
         for vocab_char in vocab.chars() {
             *vocab_letters_count.entry(vocab_char).or_insert(0) += 1;
         }
-
+        if is_subset(&start_word_letters_count, &vocab_letters_count) {
+            filtered_vocabs.push(vocab.to_string());
+        }
     }
 
-    println!("{:?}", vocabs);
+    // println!("{:?}", vocabs);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashMap;
 
     // #[test]
     // fn test_process_string() {
@@ -62,4 +77,28 @@ mod tests {
     // }
 
     // TODO cercare il modo in cui si verifica che fallisca
+
+    #[test]
+    fn subset_true() {
+        let mut first: HashMap<char, u32> = HashMap::new();
+        first.insert('a', 2);
+        first.insert('b', 1);
+
+        let mut second: HashMap<char, u32> = HashMap::new();
+        second.insert('a', 1);
+        second.insert('b', 1);
+        assert_eq!(is_subset(&first, &second), true);
+    }
+
+    #[test]
+    fn subset_false() {
+        let mut first: HashMap<char, u32> = HashMap::new();
+        first.insert('a', 2);
+        first.insert('b', 1);
+
+        let mut second: HashMap<char, u32> = HashMap::new();
+        second.insert('a', 3);
+        second.insert('b', 1);
+        assert_eq!(is_subset(&first, &second), false);        
+    }
 }
